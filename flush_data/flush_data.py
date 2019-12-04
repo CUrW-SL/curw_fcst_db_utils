e40372f08b50c0ce860503757c57ebcc0abb4cf7
shadhini <jshadhiniaseka@gmail.com>
2019-12-04 17:54:51 +0530

    add scripts to flush flo2d  hourly run data

---
+++ flush_data/flush_data.py
@@ -0,0 +1,115 @@
+#!/home/uwcc-admin/curw_fcst_MME_utils/venv/bin/python3
+import traceback
+
+from db_adapter.base import get_Pool, destroy_Pool
+from db_adapter.constants import CURW_FCST_USERNAME, CURW_FCST_PORT, CURW_FCST_PASSWORD, CURW_FCST_HOST, \
+    CURW_FCST_DATABASE
+from db_adapter.curw_fcst.timeseries import Timeseries
+
+
+def get_curw_fcst_hash_ids(pool, sim_tag=None, source_id=None, variable_id=None, unit_id=None, station_id=None,
+                           start=None, end=None):
+
+    pre_sql_statement = "SELECT `id` FROM `run` WHERE "
+
+    condition_list = []
+    variable_list = []
+
+    score = 0
+
+    if sim_tag is not None:
+        condition_list.append("`sim_tag`=%s")
+        variable_list.append(sim_tag)
+        score +=1
+    if source_id is not None:
+        condition_list.append("`source`=%s")
+        variable_list.append(source_id)
+        score +=1
+    if variable_id is not None:
+        condition_list.append("`variable`=%s")
+        variable_list.append(variable_id)
+        score +=1
+    if unit_id is not None:
+        condition_list.append("`unit`=%s")
+        variable_list.append(unit_id)
+        score +=1
+    if station_id is not None:
+        condition_list.append("`station`=%s")
+        variable_list.append(station_id)
+        score +=1
+    if start is not None:
+        condition_list.append("`start_date`=%s")
+        variable_list.append(start)
+        score +=1
+    if end is not None:
+        condition_list.append("`end_date`=%s")
+        variable_list.append(end)
+        score +=1
+
+    if score == 0:
+        return None
+
+    conditions = " AND ".join(condition_list)
+
+    sql_statement = pre_sql_statement + conditions + ";"
+
+    print(sql_statement)
+
+    ids = []
+    connection = pool.connection()
+    try:
+        with connection.cursor() as cursor:
+            row_count = cursor.execute(sql_statement, tuple(variable_list))
+            if row_count > 0:
+                results = cursor.fetchall()
+                for result in results:
+                    ids.append(result.get('id'))
+        return ids
+    except Exception:
+        traceback.print_exc()
+    finally:
+        if connection is not None:
+            connection.close()
+
+
+if __name__=="__main__":
+
+    try:
+
+        pool = get_Pool(host=CURW_FCST_HOST, port=CURW_FCST_PORT, user=CURW_FCST_USERNAME, password=CURW_FCST_PASSWORD,
+                        db=CURW_FCST_DATABASE)
+
+        hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="daily_run", source_id=None,
+                                          variable_id=None, unit_id=None, station_id=None,
+                                          start=None, end=None)
+
+        TS = Timeseries(pool=pool)
+
+        ################################################################################
+        # delete as a whole (entry in run table and all related entries in data table) #
+        ################################################################################
+        # count = 0
+        # for id in hash_ids:
+        #     TS.delete_all_by_hash_id(id_=id)
+        #     count+=1
+        #     print(count, id)
+        # print("{} of hash ids are deleted".format(len(hash_ids)))
+
+        ###################################################################################
+        # delete a specific timeseries defined by a given hash id and fgt from data table #
+        ###################################################################################
+        # fgt = "2019-10-17 19:52:00"
+        # count = 0
+        # for id in hash_ids:
+        #     TS.delete_timeseries(id_=id, fgt=fgt)
+        #     count += 1
+        #     print(count, id)
+        #
+        # print("{} of hash ids are deleted".format(len(hash_ids)))
+
+    except Exception as e:
+        print('An exception occurred.')
+        traceback.print_exc()
+    finally:
+        print("Process finished")
+        destroy_Pool(pool=pool)
---
+++ flush_data/flush_flo2d_hourly_run_outdated_data.py
@@ -0,0 +1,83 @@
+#!/home/uwcc-admin/curw_fcst_MME_utils/venv/bin/python3
+import traceback
+from datetime import datetime, timedelta
+
+from db_adapter.constants import set_db_config_file_path
+from db_adapter.constants import connection
+from db_adapter.base import get_Pool, destroy_Pool
+from db_adapter.curw_fcst.common import get_distinct_fgts_for_given_id, get_curw_fcst_hash_ids
+from db_adapter.curw_fcst.timeseries import Timeseries
+
+# from db_adapter.constants import CURW_FCST_USERNAME, CURW_FCST_PORT, CURW_FCST_PASSWORD, CURW_FCST_HOST, \
+#     CURW_FCST_DATABASE
+
+FLO2D_250_ID = 9
+FLO2D_150_ID = 10
+
+
+def select_fgts_older_than_month(fgts):
+
+    select_fgts = []
+
+    deadline = datetime.now() - timedelta(days=30)
+
+    for fgt in fgts:
+        if fgt < deadline:
+            select_fgts.append(fgt)
+
+    return select_fgts
+
+
+def flush_timeseries(pool, hash_ids):
+
+    TS = Timeseries(pool=pool)
+
+    ###################################################################################
+    # delete a specific timeseries defined by a given hash id and fgt from data table #
+    ###################################################################################
+    count = 0
+    for id in hash_ids:
+        fgts = get_distinct_fgts_for_given_id(pool=pool, id_=id)
+
+        outdated_fgts = select_fgts_older_than_month(fgts)
+        count += 1
+        for fgt in outdated_fgts:
+            fgts.remove(fgt)
+            TS.delete_timeseries(id_=id, fgt=fgt)
+            print(count, id, fgt)
+
+        TS.update_start_date(id_=id, start_date=min(fgts), force=True)
+
+    print("{} of hash ids are deleted.".format(count))
+
+
+if __name__=="__main__":
+
+    try:
+
+        set_db_config_file_path('/home/uwcc-admin/curw_fcst_MME_utils/db_adapter_config.json')
+
+        pool = get_Pool(host=connection.CURW_FCST_HOST, port=connection.CURW_FCST_PORT, user=connection.CURW_FCST_USERNAME,
+                        password=connection.CURW_FCST_PASSWORD, db=connection.CURW_FCST_DATABASE)
+
+        # pool = get_Pool(host=CURW_FCST_HOST, port=CURW_FCST_PORT,
+        #                 user=CURW_FCST_USERNAME, password=CURW_FCST_PASSWORD, db=CURW_FCST_DATABASE)
+
+        flo2d_250_hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="hourly_run", source_id=FLO2D_250_ID,
+                                          variable_id=None, unit_id=None, station_id=None,
+                                          start=None, end=None)
+
+        flush_timeseries(pool=pool, hash_ids=flo2d_250_hash_ids)
+
+        flo2d_150_hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="hourly_run", source_id=FLO2D_150_ID,
+                                                    variable_id=None, unit_id=None, station_id=None,
+                                                    start=None, end=None)
+
+        flush_timeseries(pool=pool, hash_ids=flo2d_150_hash_ids)
+
+    except Exception as e:
+        print('An exception occurred.')
+        traceback.print_exc()
+    finally:
+        print("Process finished")
+        destroy_Pool(pool=pool)
