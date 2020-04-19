8e634abc232b0598c174f17fd8816680e613027a
shadhini <jshadhiniaseka@gmail.com>
2020-04-19 17:55:36 +0530

    add script to flush wrf data older than 2 months from database

--- flush_data/flush_wrf_outdated_data.py
+++ flush_data/flush_wrf_outdated_data.py
@@ -105,6 +105,16 @@ def flush_timeseries_given_range(pool, hash_ids, start, end):
     print("{} of hash ids are deleted.".format(count))
 
 
+"""
+gfs_d0_00      
+gfs_d0_18  
+dwrf_gfs_d1_06    @22
+dwrf_gfs_d1_12    @02
+dwrf_gfs_d1_18    @09
+dwrf_gfs_d1_00    @17
+mwrf_gfs_d0_18    @02
+"""
+
 if __name__=="__main__":
 
     try:
@@ -117,29 +127,18 @@ if __name__=="__main__":
         # pool = get_Pool(host=CURW_FCST_HOST, port=CURW_FCST_PORT,
         #                 user=CURW_FCST_USERNAME, password=CURW_FCST_PASSWORD, db=CURW_FCST_DATABASE)
 
-        wrf_A_hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="gfs_d0_00", source_id=WRF_A_ID_4_1_2,
-                                          variable_id=None, unit_id=None, station_id=None,
-                                          start=None, end=None)
 
-        flush_timeseries_outdated(pool=pool, hash_ids=wrf_A_hash_ids)
+        sim_tag_list = ["gfs_d0_00", "gfs_d0_18",
+                        "dwrf_gfs_d1_06", "dwrf_gfs_d1_12", "dwrf_gfs_d1_18", "dwrf_gfs_d1_00",
+                        "mwrf_gfs_d0_18"]
 
-        wrf_C_hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="gfs_d0_00", source_id=WRF_C_ID_4_1_2,
-                                                variable_id=None, unit_id=None, station_id=None,
-                                                start=None, end=None)
+        for sim_tag in sim_tag_list:
+            print("sim_tag:", sim_tag)
+            wrf_hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag=sim_tag, source_id=None,
+                                                    variable_id=None, unit_id=None, station_id=None,
+                                                    start=None, end=None)
 
-        flush_timeseries_outdated(pool=pool, hash_ids=wrf_C_hash_ids)
-
-        wrf_E_hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="gfs_d0_00", source_id=WRF_E_ID_4_1_2,
-                                                variable_id=None, unit_id=None, station_id=None,
-                                                start=None, end=None)
-
-        flush_timeseries_outdated(pool=pool, hash_ids=wrf_E_hash_ids)
-
-        wrf_SE_hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="gfs_d0_00", source_id=WRF_SE_ID_4_1_2,
-                                                variable_id=None, unit_id=None, station_id=None,
-                                                start=None, end=None)
-
-        flush_timeseries_outdated(pool=pool, hash_ids=wrf_SE_hash_ids)
+            flush_timeseries_outdated(pool=pool, hash_ids=wrf_hash_ids)
 
 
     except Exception as e:
